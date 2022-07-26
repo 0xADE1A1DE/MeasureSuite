77f6aef18c7dfc526ebd6bafad0d3b2ffea9c780
Joel <rootjdev@gmail.com>
2022-07-26 15:48:39 +0930

    module

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,4 +1,7 @@
 import typescript from "@rollup/plugin-typescript";
+import copy from "rollup-plugin-copy";
+
+import dts from 'rollup-plugin-dts'
 
 const external = ["lodash-es", "measure-addon", "child_process", "fs", "os", "path"];
 export default [
@@ -9,6 +12,23 @@ export default [
       format: "es",
     },
     external,
-    plugins: [typescript()],
+    plugins: [typescript(),
+    copy({
+      targets: [
+        {
+          src: [
+            "build/Release/measuresuite.node",
+          ],
+          dest: "./dist",
+        },
+      ],
+    }),
+    ],
+  },
+  {
+    // path to your declaration files root
+    input: './dist/dts/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'es' }],
+    plugins: [dts()],
   },
 ];
