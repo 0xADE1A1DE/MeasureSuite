d912772d4f31cd23b0b47297971caada76a0b12c
Joel <rootjdev@gmail.com>
2022-08-15 09:27:41 +0930

    double checking mmap

--- src/lib/timer.c
+++ src/lib/timer.c
@@ -41,6 +41,10 @@ static void init_fdperf() {
     return;
   }
   buf = mmap(NULL, sysconf(_SC_PAGESIZE), PROT_READ, MAP_SHARED, fdperf, 0);
+  // NOLINTNEXTLINE (mmap - api)
+  if (buf == MAP_FAILED) {
+    fdperf = -1;
+  }
 }
 
 static void measuresuite_time_pmc(uint64_t *t) {
