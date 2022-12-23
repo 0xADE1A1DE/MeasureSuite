b86ef8df6c6a1ab13c4c66d6e411b9f387ae871e
Joel <rootjdev@gmail.com>
2022-12-23 14:11:35 +1030

    fixed json gen

--- src/lib/json.c
+++ src/lib/json.c
@@ -1,76 +1,89 @@
 #include "json.h"
 #include "alloc_helper.h"
 #include "struct_measuresuite.h"
+
+#define PRINT(...)                                                             \
+  size = json_end - json;                                                      \
+  len = snprintf(json, size, ##__VA_ARGS__);                                   \
+  if (len >= size) {                                                           \
+    goto enlarge;                                                              \
+  }                                                                            \
+  json += len;
+
 int generate_json(struct measuresuite *ms, uint64_t delta_in_seconds,
                   size_t check_result) {
   char *json = ms->json;
-  char *json_end = ms->json + ms->json_len;
+  char *json_end = ms->json + ms->json_len - 1;
 
-  json += snprintf(json, ms->json_len,
-                   "{\"stats\":"
-                   "{"
-                   "\"numFunctions\":%" PRIu64 ","
-                   "\"runtime\":%" PRIu64 "," // in seconds
-                   "\"incorrect\":%" PRIu64
-                   "}," // if 0, ok, otherwise the index of which function is
-                        // incorrect to the previous one.
-                   "\"functions\":[",
-                   ms->num_functions, delta_in_seconds, check_result);
+  size_t size = json_end - json; // available
+  size_t len = 0;                // written
+
+  PRINT("{\"stats\":"
+        "{"
+        "\"numFunctions\":%" PRIu64 ","
+        "\"runtime\":%" PRIu64 "," // in seconds
+        "\"incorrect\":%" PRIu64
+        "}," // if incorrect is '0', all functions calculate the same, otherwise
+             // the index of which function is incorrect to the previous one.
+        "\"functions\":[",
+        ms->num_functions, delta_in_seconds, check_result);
 
   // print function meta data
-  FOR_EACH_FUNCTION {
+  //
 
+  for (struct function_tuple *fct = ms->functions;
+       fct <= &ms->functions[ms->num_functions - 1]; fct++) {
     switch (fct->type) {
     case ASM:
-      json +=
-          snprintf(json, json_end - json,
-                   "{\"type\":\"ASM\", \"chunks\":%" PRIi32 "},", fct->chunks);
+      PRINT("{\"type\":\"ASM\", \"chunks\":%" PRIi32 "},", fct->chunks);
       break;
     case BIN:
-      json += snprintf(json, json_end - json, "{\"type\":\"BIN\"},");
+      PRINT("{\"type\":\"BIN\"},");
       break;
     case ELF:
-      json += snprintf(json, json_end - json, "{\"type\":\"ELF\"},");
+      PRINT("{\"type\":\"ELF\"},");
       break;
     case SHARED_OBJECT:
-      json += snprintf(json, json_end - json, "{\"type\":\"SHARED_OBJECT\"},");
+      PRINT("{\"type\":\"SHARED_OBJECT\"},");
       break;
     }
   }
+
   // overwrite comma
   json--;
-  json += snprintf(json, json_end - json, "],\"cycles\":[");
+  PRINT("],\"cycles\":[");
 
   // print cycles
-  FOR_EACH_FUNCTION {
+  for (struct function_tuple *fct = ms->functions;
+       fct <= &ms->functions[ms->num_functions - 1]; fct++) {
 
-    json += snprintf(json, json_end - json, "[");
+    PRINT("[");
     for (size_t run_i = 0; run_i < ms->num_batches; run_i++) {
-      json += snprintf(json, json_end - json, "%" PRIu64 ",",
-                       fct->cycle_results[run_i]);
+      PRINT("%" PRIu64 ",", fct->cycle_results[run_i]);
     }
     // overwrite comma after last number
     json--;
     // close array and comma for next one
-    json += snprintf(json, json_end - json, "],");
+    PRINT("],");
   }
 
   // overwrite comma after last array
   json -= 1;
-  json += snprintf(json, json_end - json, "]}");
-
-  if (json_end - json <= 0) {
-    // we did not have enough space.
-
-    // enlarge
-    ms->json_len *= 2;
-    if (realloc_or_fail(ms, (void **)&(ms->json), ms->json_len)) {
-      return 1;
-    }
-
-    // and try again recursively
-    return generate_json(ms, delta_in_seconds, check_result);
-  }
+  PRINT("]}");
 
   return 0;
+
+  // we did not have enough space.
+enlarge:
+
+  // enlarge
+  ms->json_len *= 2;
+  fprintf(stderr, "trying to realloc space for more json\n");
+  if (realloc_or_fail(ms, (void **)&(ms->json), ms->json_len)) {
+    return 1;
+  }
+
+  // and try again recursively
+  return generate_json(ms, delta_in_seconds, check_result);
 }
+#undef PRINT
