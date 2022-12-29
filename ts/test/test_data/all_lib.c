0a502ba12ad6f763b78211a01c8f503b94c3bf73
Joel <rootjdev@gmail.com>
2022-12-29 13:10:02 +1030

    lint c

--- ts/test/test_data/all_lib.c
+++ ts/test/test_data/all_lib.c
@@ -16,53 +16,65 @@
 
 #include <stdint.h>
 
-void increment(long *d, const long *s) { *d = *s + 1; }
+
+
+void increment(long *dest, const long *src) { *dest = *src + 1; }
 
 // reminder of calling convention:
 // https://en.wikipedia.org/wiki/X86_calling_conventions#List_of_x86_calling_conventions
 // RDI, RSI, RDX, RCX, R8, R9, [XYZ]MM0–7
 
-void sixparamadd(uint64_t *o0, uint64_t *o1, uint64_t *o2, const uint64_t *i0,
-                 const uint64_t *i1, const uint64_t *i2) {
-  *o0 = *i0 + 1;
-  *o1 = *i1 + 1;
-  *o2 = *i2 + 1;
+void six_param_add1(uint64_t *out0, uint64_t *out1, uint64_t *out2,
+                    const uint64_t *in0, const uint64_t *in1,
+                    const uint64_t *in2) {
+  *out0 = *in0 + 1;
+  *out1 = *in1 + 1;
+  *out2 = *in2 + 1;
 }
 
-void array_width_three_add(uint64_t o[3], const uint64_t i[3]) {
-  *o++ = *i++ + 1;
-  *o++ = *i++ + 1;
-  *o = *i + 1;
+void array_width_three_add(uint64_t out0[3], const uint64_t in0[3]) {
+  *out0++ = *in0++ + 1;
+  *out0++ = *in0++ + 1;
+  *out0 = *in0 + 1;
 }
 
-void add_two_numbers(uint64_t *o, const uint64_t *i0, const uint64_t *i1) {
-  *o = *i0 + *i1;
+void add_two_numbers(uint64_t *out0, const uint64_t *in0, const uint64_t *in1) {
+  *out0 = *in0 + *in1;
 }
 
-void array_width_three_mul(uint64_t o[3], const uint64_t i0[3],
-                           const uint64_t i1[3]) {
-  o[0] = i0[0] * i1[0];
-  o[1] = i0[1] * i1[1];
-  o[2] = i0[2] * i1[2];
+void broadcast_2(uint64_t *out0, uint64_t *out1, const uint64_t *in0) {
+  *out0 = *in0;
+  *out1 = *in0;
+}
+void broadcast_array_2(uint64_t out0[2], const uint64_t *in0) {
+  out0[0] = *in0;
+  out0[1] = *in0;
+}
+
+void array_width_three_mul(uint64_t out0[3], const uint64_t in0[3],
+                           const uint64_t in1[3]) {
+  out0[0] = in0[0] * in1[0];
+  out0[1] = in0[1] * in1[1];
+  out0[2] = in0[2] * in1[2];
   /** *o++ = *i0++ * *i1++; */
   /** *o++ = *i0++ * *i1++; */
   /** *o = *i0 * *i1; */
 }
 
-void array_width_three_add_pair(uint64_t o[3], const uint64_t i0[3],
-                                const uint64_t i1[3]) {
-  o[0] = i0[0] + i1[0];
-  o[1] = i0[1] + i1[1];
-  o[2] = i0[2] + i1[2];
+void array_width_three_add_pair(uint64_t out0[3], const uint64_t in0[3],
+                                const uint64_t in1[3]) {
+  out0[0] = in0[0] + in1[0];
+  out0[1] = in0[1] + in1[1];
+  out0[2] = in0[2] + in1[2];
 }
 
-void array_width_five_add_pair(uint64_t *o, const uint64_t *i0,
-                               const uint64_t *i1) {
-  o[0] = i0[0] + i1[0];
-  o[1] = i0[1] + i1[1];
-  o[2] = i0[2] + i1[2];
-  o[3] = i0[3] + i1[3];
-  o[4] = i0[4] + i1[4];
+void array_width_five_add_pair(uint64_t *out, const uint64_t *in0,
+                               const uint64_t *in1) {
+  out[0] = in0[0] + in1[0];
+  out[1] = in0[1] + in1[1];
+  out[2] = in0[2] + in1[2];
+  out[3] = in0[3] + in1[3];
+  out[4] = in0[4] + in1[4];
 }
 void mul2(uint64_t out1[4], uint64_t out2[4], const uint64_t arg1[4],
           const uint64_t arg2[4], const uint64_t arg3[4],
